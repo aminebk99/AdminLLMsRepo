4f8792482da90186517b1082f0ffb59eca8107fe
aminebk99 <129026688+aminebk99@users.noreply.github.com>
2023-10-18 10:28:32 +0100

    update

--- src/app.py
+++ src/app.py
@@ -1,5 +1,7 @@
 from init import app
+import os
 
+app.secret_key = os.environ.get('SECRET_KEY')
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
 
--- src/init/routes/template_route.py
+++ src/init/routes/template_route.py
@@ -1,11 +1,16 @@
 # init/routes/template_route.py
 from flask import Blueprint, redirect
 from authlib.integrations.flask_client import OAuth
+from init import controllers
 from ..config import Config
 
+# Define the blueprint for the template routes
 template_route = Blueprint('template', __name__)
+
+# Initialize OAuth
 oauth = OAuth()
 
+# Configure the GitHub OAuth provider
 def configure_github_oauth():
     github = oauth.register(
         name='github',
@@ -17,13 +22,13 @@ def configure_github_oauth():
     )
     return github
 
-@template_route.route('/login/github_oauth', methods=['GET'])
+# Route for initiating the GitHub login
+@template_route.route('/login/github', methods=['GET'])
 def login_with_github():
     github = configure_github_oauth()
     return redirect(github.authorize_redirect())
 
-
-
+# Route for handling the GitHub login callback
 @template_route.route('/login/callback/github', methods=['GET'])
 def call_back_login_with_github():
     github = configure_github_oauth()
@@ -32,26 +37,22 @@ def call_back_login_with_github():
     # Handle user data as needed
     return f'Hello, {user["sub"]}'
 
-@template_route.route('/login/huggingface', methods=['POST'])
-def login_with_huggingface():
-    return controller.login_with_huggingface()
+# @template_route.route('/models', methods=['GET'])
+# def fetch_models_from_huggingface():
+#     return controllers.fetch_models_from_huggingface()
 
-@template_route.route('/models', methods=['GET'])
-def fetch_models_from_huggingface():
-    return controller.fetch_models_from_huggingface()
+# @template_route.route('/docker-image/<int:model_id>', methods=['POST'])
+# def create_docker_image(model_id):
+#     return controller.create_docker_image(model_id)
 
-@template_route.route('/docker-image/<int:model_id>', methods=['POST'])
-def create_docker_image(model_id):
-    return controller.create_docker_image(model_id)
+# @template_route.route('/github/repos', methods=['GET'])
+# def fetch_github_repos():
+#     return controller.fetch_github_repos()
 
-@template_route.route('/github/repos', methods=['GET'])
-def fetch_github_repos():
-    return controller.fetch_github_repos()
+# @template_route.route('/template', methods=['POST'])
+# def create_cloud_llm_template():
+#     return controller.create_cloud_llm_template()
 
-@template_route.route('/template', methods=['POST'])
-def create_cloud_llm_template():
-    return controller.create_cloud_llm_template()
-
-@template_route.route('/template/<int:template_id>', methods=['GET'])
-def get_template(template_id):
-    return controller.get_template(template_id)
+# @template_route.route('/template/<int:template_id>', methods=['GET'])
+# def get_template(template_id):
+#     return controller.get_template(template_id)
