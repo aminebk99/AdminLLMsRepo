cf78750c4b52ce417ab423c23fff914ec4efd272
Mohamed-Amine-Bazaou <143953485+Mohamed-Amine-Bazaou@users.noreply.github.com>
2023-10-30 14:29:42 +0100

    update model LLMTemplate

--- src/init/models/LLMTemplate.py
+++ src/init/models/LLMTemplate.py
@@ -12,20 +12,20 @@ class LLMTemplate(db.Model):
     name = db.Column(db.String, nullable=False)
     description = db.Column(db.String, nullable=False)
     docker_image_url = db.Column(db.String, nullable=False)
-    type = db.Column(db.Enum(LLMType), nullable=False)
+    type = db.Column(db.Enum(LLMType), nullable=False, default=LLMType.OS_LLM)  
     uuid = db.Column(db.String, nullable=False, unique=True, default=str(uuid.uuid4()))
-    version = db.Column(db.String, nullable=False)
+    version = db.Column(db.String, nullable=False, default='1.0') 
     is_active = db.Column(db.Boolean, nullable=False, default=True)
     tags = db.Column(db.String, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
-def to_dict(self):
+    def to_dict(self):
         return {
             'id': self.id,
             'name': self.name,
             'description': self.description,
             'docker_image_url': self.docker_image_url,
-            'type': self.type.value, 
+            'type': self.type.value,
             'uuid': self.uuid,
             'version': self.version,
             'is_active': self.is_active,
